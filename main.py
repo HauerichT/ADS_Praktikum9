14237c4c741fa3c57ea09616fbfa87e96eed66ea
HauerichT <timo.haverich@fh-bielefeld.de>
2023-06-20 21:46:42 +0200

    init

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Node:
     def __init__(self, key):
         self.key = key
@@ -18,7 +21,11 @@ class HashTable:
                 cur = cur.next
             cur.next = Node(key)
 
+    def delete(self, key):
+        print("Delete", key)
+
     def printHashTable(self):
+        print("\n--- Hash-Tabelle ---")
         for j in range(len(self.hashTable)):
             if self.hashTable[j] is not None:
                 print("Index " + str(j) + ": ", end="")
@@ -31,17 +38,61 @@ class HashTable:
                 print("Index " + str(j))
 
 
+def readFile():
+    try:
+        with open('data.txt', 'r') as f:
+            dataInFile = f.read()
+            arrFileData = dataInFile.split(",")
+            for s in range(len(arrFileData)):
+                arrFileData[s] = arrFileData[s].strip()
+            return arrFileData
+    except FileNotFoundError:
+        print("Auslesen der Datei fehlgeschlagen!")
+    finally:
+        f.close()
+        print("Daten aus data.txt eingelesen.\n")
+
+
 # Driver code
 if __name__ == '__main__':
-    # Create a hash table with
-    # a capacity of 5
     ht = HashTable()
+    # Startzeitpunkt: einlesen und eintragen der 50 Datensätze
+    startTime = datetime.now()
+    # 50 Datensätze initial einlesen
+    filedata = readFile()
+    # Datensätze in Hash Tabelle eintragen
+    for i in filedata:
+        ht.insert(i)
+    # Endzeitpunkt: einlesen und eintragen der 50 Datensätze
+    endTime = datetime.now()
+    # Benötigte Zeit ausgeben
+    diffTime = (endTime - startTime).total_seconds()
+    print("Zeit einlesen und eintragen 50 Datensätze:", diffTime, "Sekunden")
 
-    with open('data.txt', 'r') as f:
-        dataInFile = f.read()
-        arrFileData = dataInFile.split(",")
-        for s in arrFileData:
-            element = s.strip()
-            ht.insert(element)
+    # Menü
+    while True:
+        print("\n--- Menü ---")
+        print("1. Datensatz hinzufügen")
+        print("2. Datensatz löschen")
+        print("3. Hashtabelle anzeigen")
+        print("4. Hashtabelle speichern")
+        print("5. Beenden")
 
-    ht.printHashTable()
+        choice = input("Auswahl: ")
+
+        if choice == "1":
+            record = input("Geben Sie den neuen Datensatz ein: ")
+            ht.insert(record)
+            print("Datensatz wurde hinzugefügt.")
+        elif choice == "2":
+            record = input("Geben Sie den zu löschenden Datensatz ein (Key): ")
+            ht.delete(record)
+            print("Datensatz wurde gelöscht.")
+        elif choice == "3":
+            ht.printHashTable()
+        elif choice == "4":
+            print("Speichern")
+        elif choice == "5":
+            break
+        else:
+            print("Ungültige Auswahl.")
